3bd532cebafdcf6daa5f75abac68d736b32491e2
Suraj4896 <145790331+Suraj4896@users.noreply.github.com>
2024-12-11 20:05:31 +0530

    Add files via upload

---
+++ backend with Node.js/helloworld-node/index.js
@@ -0,0 +1,11 @@
+console.log("hello node js");
+
+const array = [1,2,3,4];
+console.log(array);
+
+//delay 2 sec
+setTimeout(() => {
+    console.log("this is node js concepts");
+}, 2000);
+
+console.log("this is last line of code");
